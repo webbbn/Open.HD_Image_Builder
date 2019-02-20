ff58eb53f4b87b19699c71b9e67dbd67b1e2b383
root <root@devubuntu>
2019-02-20 12:40:00 -0800

    Baby steps

--- stages/00-Prerequisites/NANOPICORE2/00-run.sh
+++ stages/00-Prerequisites/NANOPICORE2/00-run.sh
@@ -1,14 +1,17 @@
 log "Checking prerequisites"
 
-pushd "${STAGE_DIR}"
+pushd "${STAGE_WORK_DIR}"
 
 if [ ! -d ~/nanopitools ]; then
+    log "Download NanoPi Tools from Google Drive"
+    ../../scripts/gdown.pl https://drive.google.com/open?id=1MZ4gXAISbSPtfhUQJyUEVRvrE56jBYQ2 ./gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
+
     log "Extract NanoPi Tools"
-    mkdir -p "~/nanopitools"
+    mkdir -p ~/nanopitools
     tar xf "gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz" -C ~/nanopitools
 
     log "Install the NanoPi Tools"
-    echo PATH=\$PATH:~/nanopitools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin >> ~/.bashrc
+    echo PATH=\$PATH:~/nanopitools/gcc-linaro-arm-linux-gnueabihf-raspbian/bin >> ~/.bashrc
     source ~/.bashrc
 fi
 
--- stages/01-Baseimage/NANOPICORE2/00-run.sh
+++ stages/01-Baseimage/NANOPICORE2/00-run.sh
@@ -6,13 +6,15 @@ rm -f *.img
 rm -f *.zip
 
 log "Download NanoPi debian base Image"
-if [ ! -f "${STAGE_DIR}/nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip" ]; then
-  log "Visit https://drive.google.com/drive/folders/1moc57opcmlMle66bOCXgxA6OcoV7qrGh"
-  log "Download latest nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip"
-  exit 1
-fi
+../../scripts/gdown.pl https://drive.google.com/open?id=1O9JPd6wT1ICDutdq1Dk8140HdkUZv8Yf ./nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip
 
-cp "${STAGE_DIR}/nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip" "${STAGE_WORK_DIR}"
+#if [ ! -f "${STAGE_DIR}/nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip" ]; then
+#  log "Visit https://drive.google.com/drive/folders/1moc57opcmlMle66bOCXgxA6OcoV7qrGh"
+#  log "Download latest nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip"
+#  exit 1
+#fi
+
+#cp "${STAGE_DIR}/nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip" "${STAGE_WORK_DIR}"
 unzip "nanopi-neo-core2_sd_friendlycore-xenial_4.14_arm64_20181011.img.zip" 
 
 log "Rename to IMAGE.img"
